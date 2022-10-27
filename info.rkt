b4ca7ca8381dab2241012243af0ea0128c3ae734
Nathan Fritzler <nfblaster@live.com>
2022-10-26 21:38:12 -0600

    Try to fix the deps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "encode-ITA_2")
-(define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "typed-racket-lib"))
+(define deps '("base" "typed-racket-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "rackunit-typed"))
 (define scribblings '(("scribblings/encode-ITA_2.scrbl" ())))
 (define pkg-desc "Encodes strings in the ITA2 character encoding.")
 (define version "0.0")
